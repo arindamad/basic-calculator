1f64e5b19efc66946b723555550b3f7ca22e5cc3
sguha-work <sguha1988.life@gmail.com>
2018-09-29 10:53:44 +0530

    coding for calculator

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-var calVal="";
-$('button').on('click', function(){
-  var numVal=$(this).val();
-  calVal+=numVal;
-  $('.rasult').text(calVal);
-  console.log(calVal);
+var calVal = "";
+var operation = "";
+$('button.operation').on('click', function() {
+  operation = $(this).val();
+});
+$('button').on('click', function () {
+  if ($(this).val() != '=') {
+    var numVal = $(this).val();
+    calVal += numVal;
+    $('.rasult').text(calVal);
+    console.log(calVal);
+  } else {
+    if(operation != '') {
+      switch(operation) {
+        case '+':
+          var calValSplited = calVal.split('+');
+          calVal = parseInt(calValSplited[0]) + parseInt(calValSplited[1]);
+          $('.rasult').text(calVal);
+        break;
+        case '-':
+          var calValSplited = calVal.split('-');
+          calVal = parseInt(calValSplited[0]) - parseInt(calValSplited[1]);
+          $('.rasult').text(calVal);
+        break;
+        case '*':
+          var calValSplited = calVal.split('*');
+          calVal = parseInt(calValSplited[0]) * parseInt(calValSplited[1]);
+          $('.rasult').text(calVal);
+        break;
+        case '/':
+          var calValSplited = calVal.split('/');
+          calVal = parseInt(calValSplited[0]) / parseInt(calValSplited[1]);
+          $('.rasult').text(calVal);
+        break;
+
+      }
+    }
+  }
+
 });
